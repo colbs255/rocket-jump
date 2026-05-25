90cfc75add151af40ce0f1dd9c51938df4024777
colbs255 <colbs255@users.noreply.github.com>
2026-05-25 00:24:55 -0400

    save

--- components/player/player.gd
+++ components/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 130.0
-const JUMP_VELOCITY = -300.0
+const JUMP_SPEED = 500.0
 const BULLET_VELOCITY = 200
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
@@ -18,7 +18,7 @@ func _physics_process(delta: float) -> void:
 
     # Handle jump.
     if Input.is_action_just_pressed("jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+        velocity.y = -JUMP_SPEED
 
     # Input direction: -1 ->  left, 0 -> none, 1 -> right
     var direction := Input.get_axis("move_left", "move_right")
